df7240b855b3ac6446a892145bcaab482f6b0f38
Hoangduy2982003 <125873702+Hoangduy2982003@users.noreply.github.com>
2024-04-15 19:41:58 +0700

    Init All

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello NodeJs");
+import {sumTwoNum , printConsole} from "./util.js";
+// import subTwoNum from "./util.js";
+import {default as subTwoNum } from "./util.js";
+
+function main() {
+    printConsole("Tong 2 so 1 va 2 la :" + sumTwoNum(1,2));
+    printConsole("Hieu 2 so 1 va 2 la :" + subTwoNum(1,2));
+}
+
+main();
